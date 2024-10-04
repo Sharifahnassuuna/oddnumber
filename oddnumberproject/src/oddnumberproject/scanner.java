0392c4993c64eac1896b1793be7fb94dde5e3daa
sam Ray <85815217+katosamuelraymond@users.noreply.github.com>
2024-10-04 12:48:44 +0100

    first commit

---
+++ oddnumberproject/src/oddnumberproject/scanner.java
@@ -0,0 +1,15 @@
+package oddnumberproject;
+
+public class scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int nextInt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
